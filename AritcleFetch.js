5867061e2cc85049042e1ac95f754edb391a277b
Austin Paul <safeat3rd@hotmail.com>
2018-02-10 21:28:25 -0600

    Updated ArticleFetch API call

--- AritcleFetch.js
+++ AritcleFetch.js
@@ -1,24 +1,29 @@
 
 //New York Times Article Search API
 function call_API(keyword){
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-    'api-key': process.env.KEY,
-    'q': keyword,
-    'page': 0
-    });
-    $.ajax({
-    url: url,
-    method: 'GET',
-    }).done(function(result) {
-    return result;
-    }).fail(function(err) {
-    throw err;
-    });
-}
+   // var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    fetch("https://api.nytimes.com/svc/search/v2/articlesearch.json").then(function(keyword) {
+        url += '?' + $.param({
+            'api-key': process.env.KEY,
+            'q': keyword,
+            'page': 0
+            });
+            $.ajax({
+            url: url,
+            method: 'GET',
+            }).done(function(result) {
+            return result;
+            }).fail(function(err) {
+            throw err;
+            });
+        }
+    
+    );
+    
 var JSON_file = call_API(keyword);
 
 function parseJSON(JSON_file) {
     var file = JSON.parse(JSON_file);
     
-} 
+}
+}
