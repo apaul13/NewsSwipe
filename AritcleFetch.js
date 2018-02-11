90625656ca9ce2e6d08978f573ecfefaf4df7ee0
Austin Paul <safeat3rd@hotmail.com>
2018-02-11 04:53:02 -0600

    Small Update

--- AritcleFetch.js
+++ AritcleFetch.js
@@ -1,30 +1,48 @@
 
 //New York Times Article Search API
 function call_API(keyword){
-   // var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    fetch("https://api.nytimes.com/svc/search/v2/articlesearch.json").then(function(keyword) {
-        url += '?' + $.param({
-            'api-key': process.env.KEY,
-            'q': keyword,
-            'page': 0 
-            });
-            $.ajax({
-            url: url,
-            method: 'GET',
-            }).done(function(result) {
-            return result;
-            }).fail(function(err) {
-            throw err;
-            });
-        }
-    )
-    var file = JSON.parse(JSON_file);
-    ;
-    
+    console.log("WOOHOO")
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+    'api-key': process.env.KEY,
+    'q': keyword,
+    'page': 0
+    });
+    $.ajax({
+    url: url,
+    method: 'GET',
+    }).done(function(result) {
+        console.log(result)
+    return result;
+    }).fail(function(err) {
+    throw err;
+    });
+}
+
 var JSON_file = call_API(keyword);
 
 function parseJSON(JSON_file) {
     var file = JSON.parse(JSON_file);
     
-}
-}
+} 
+
+// function call_API(keyword){
+//     // var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+//      fetch("https://api.nytimes.com/svc/search/v2/articlesearch.json").then(function(keyword) {
+//          url += '?' + $.param({
+//              'api-key': process.env.KEY,
+//              'q': keyword,
+//              'page': 0 
+//              });
+//              $.ajax({
+//              url: url,
+//              method: 'GET',
+//              }).done(function(result) {
+//              return result;
+//              }).fail(function(err) {
+//              throw err;
+//              });
+//          }
+//      )
+//      var file = JSON.parse(JSON_file);
+//      ;
